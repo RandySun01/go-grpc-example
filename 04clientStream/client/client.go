2ac8ddf666fb82d038ccecb2dc4e853806a50775
RandySun01 <2214644978@qq.com>
2022-05-07 20:34:57 +0800

    Add client stream

---
+++ 04clientStream/client/client.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	pb "go-grpc-example/04clientStream/proto"
+	"io"
+	"log"
+	"strconv"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+/*
+@author RandySun
+@create 2022-05-07-20:06
+*/
+// Address 连接地址
+const Address string = ":8000"
+
+var streamClient pb.StreamClientClient
+
+// routeList 调用服务端RouteList方法
+func routeList() {
+	// 调用服务端RouteList方法,获取流
+	stream, err := streamClient.RouteList(context.Background())
+	if err != nil {
+		log.Fatalf("Upload list err: %v", err)
+	}
+
+	for n := 0; n < 5; n++ {
+		//向流中发送消息
+		err := stream.Send(&pb.StreamRequest{
+			StreamData: "stream client rpc " + strconv.Itoa(n),
+		})
+		// 添加判断服务端是否停止接口数据
+		//发送也要检测EOF，当服务端在消息没接收完前主动调用SendAndClose()关闭stream，此时客户端还执行Send()，则会返回EOF错误，所以这里需要加上io.EOF判断
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("stream request err: %v", err)
+		}
+	}
+	// 关闭流并获取返回消息
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("RouteList get response err: %v", err)
+	}
+	log.Println(res)
+}
+
+func main() {
+	// 连接服务器
+	conn, err := grpc.Dial(Address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("net.Connect err: %v", err)
+	}
+	defer conn.Close()
+	// 建立gRPC连接
+	streamClient = pb.NewStreamClientClient(conn)
+	routeList()
+}
---
+++ 04clientStream/server/server.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	pb "go-grpc-example/04clientStream/proto"
+	"io"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+/*
+@author RandySun
+@create 2022-05-07-20:06
+*/
+// SimpleService 定义服务
+type SimpleService struct {
+}
+
+//RouteList 实现RouteList方法
+func (s *SimpleService) RouteList(srv pb.StreamClient_RouteListServer) error {
+	for {
+		// 从流中获取信息
+		res, err := srv.Recv()
+		if err == io.EOF {
+			// 发送结果并关闭
+			return srv.SendAndClose(&pb.SimpleResponse{
+				Code:  200,
+				Value: "ok",
+			})
+		}
+		if err != nil {
+			return err
+		}
+		// 打印结果
+		log.Println(res.StreamData)
+		// 接收一次就停止接收数据
+		return srv.SendAndClose(&pb.SimpleResponse{Value: "ok"})
+
+	}
+}
+
+const (
+	// Address 监听地址
+	Address string = ":8000"
+	// Network 网络通信协议
+	Network string = "tcp"
+)
+
+func main() {
+	// 监听本地端口
+	listener, err := net.Listen(Network, Address)
+	if err != nil {
+		log.Fatalf("net.Listen err: %v", err)
+	}
+	log.Println(Address + "net.Listing...")
+	// 创建gRPC服务器实例
+	grpcServer := grpc.NewServer()
+	// gRPC服务器注册服务
+	pb.RegisterStreamClientServer(grpcServer, &SimpleService{})
+
+	//用服务器 Serve() 方法以及我们的端口信息区实现阻塞等待，直到进程被杀死或者 Stop() 被调用
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("grpcServer.Serve err: %v", err)
+	}
+}
