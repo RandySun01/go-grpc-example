6e9c170228d987b4b7a22a53ea7fffde6d099b41
RandySun01 <2214644978@qq.com>
2022-05-15 16:18:40 +0800

    Add metadata

---
+++ 16metadata/test/metadata_test.go
@@ -0,0 +1,17 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"google.golang.org/grpc/metadata"
+)
+
+/*
+@author RandySun
+@create 2022-05-15-15:44
+*/
+func TestMetadata(t *testing.T) {
+	md := metadata.New(map[string]string{"go": "programming", "tour": "book"})
+	fmt.Printf("%#v, \n%T \n ", md, md)
+}
